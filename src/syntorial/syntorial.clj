65f36570c55cb344350c3806a7772dded021ab49
Roger Allen <rallen@gmail.com>
2013-11-03 21:40:02 -0800

    add attack/release, Group 2 & On Your Own 3

--- src/syntorial/syntorial.clj
+++ src/syntorial/syntorial.clj
@@ -6,6 +6,8 @@
                            osc1-waveform   0    ; 0=saw, 1=pulse
                            osc1-pulsewidth 0.50 ;
                            cutoff          1.0  ; 0.0 - 1.0
+                           attack          0.150
+                           release         0.015
                            master-volume   0.0  ; volume in dB
                            gate            1    ; note-on/off
                            out-bus         0    ; output bus
@@ -15,7 +17,7 @@
         osc1-pulse (pulse note-freq osc1-pulsewidth)
         osc1       (select osc1-waveform [osc1-saw osc1-pulse])
         filt       (lpf osc1 (lin-exp cutoff 0.0 1.0 20.0 20000.0))
-        env1       (env-gen (asr 0.01 1.0 0.01) :gate gate :action FREE)
+        env1       (env-gen (asr attack 1.0 release) :gate gate :action FREE)
         master     (pow 10 (/ master-volume 20)) ; dB -> V
         ]
     (out out-bus (* master env1 [filt filt]))))
@@ -40,6 +42,35 @@
 
   ;; saw + lpf + master
   (def mpp (midi-poly-player (partial syntorial-synth :cutoff 0.4 :master-volume 6.0)))
+
+  ;; saw + lpf + master + attack/release
+  ;; On your own 3
+  ;; Bass
+  (def mpp (midi-poly-player (partial syntorial-synth
+                                      :osc1-waveform 1
+                                      :osc1-pulsewidth 0.4
+                                      :cutoff        0.4
+                                      :master-volume 8.0
+                                      :attack        0.005
+                                      :release       0.25
+                                      )))
+  ;; Lead
+  (def mpp (midi-poly-player (partial syntorial-synth
+                                      :osc1-waveform 0
+                                      :cutoff        0.8
+                                      :master-volume -3.0
+                                      :attack        0.005
+                                      :release       0.5
+                                      )))
+  ;; Pad
+  (def mpp (midi-poly-player (partial syntorial-synth
+                                      :osc1-waveform   1
+                                      :osc1-pulsewidth 0.15
+                                      :cutoff          0.5
+                                      :master-volume   6.0
+                                      :attack          2.5
+                                      :release         1.5
+                                      )))
 
   (midi-player-stop)
   (stop)
