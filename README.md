4d02a718a6a2fde29ec9f719e25eb52d23bb1525
Roger Allen <rallen@gmail.com>
2013-11-03 22:15:12 -0800

    Last update for tonight

--- README.md
+++ README.md
@@ -1,35 +1,12 @@
 # syntorial
 
-FIXME: description
+Working through the demo of Syntorial and coding up an overtone synth
+to match the basic synth they are describing.
 
-## Installation
-
-Download from http://example.com/FIXME.
-
-## Usage
-
-FIXME: explanation
-
-    $ java -jar syntorial-0.1.0-standalone.jar [args]
-
-## Options
-
-FIXME: listing of options this app accepts.
-
-## Examples
-
-...
-
-### Bugs
-
-...
-
-### Any Other Sections
-### That You Think
-### Might be Useful
+See http://www.syntorial.com/
 
 ## License
 
-Copyright © 2013 FIXME
+Copyright © 2013 Roger Allen
 
 Distributed under the Eclipse Public License, the same as Clojure.
--- src/syntorial/syntorial.clj
+++ src/syntorial/syntorial.clj
@@ -1,26 +1,37 @@
 (ns syntorial.syntorial
   (:use [overtone.live]))
 
-(defsynth syntorial-synth [note            60   ; midi note value
-                           amp             1.0  ; amplitude/volume
-                           osc1-waveform   0    ; 0=saw, 1=pulse
-                           osc1-pulsewidth 0.50 ;
-                           cutoff          1.0  ; 0.0 - 1.0
-                           attack          0.150
-                           release         0.015
-                           master-volume   0.0  ; volume in dB
-                           gate            1    ; note-on/off
-                           out-bus         0    ; output bus
-                           ]
-  (let [note-freq  (midicps note)
-        osc1-saw   (lf-saw note-freq)
-        osc1-pulse (pulse note-freq osc1-pulsewidth)
-        osc1       (select osc1-waveform [osc1-saw osc1-pulse])
-        filt       (lpf osc1 (lin-exp cutoff 0.0 1.0 20.0 20000.0))
-        env1       (env-gen (asr attack 1.0 release) :gate gate :action FREE)
-        master     (pow 10 (/ master-volume 20)) ; dB -> V
+(defsynth syntorial-synth
+  [note            60   ; midi note value
+   amp             1.0  ; amplitude/volume
+   osc1-waveform   0    ; 0=saw, 1=pulse
+   osc1-pulsewidth 0.50 ;
+   osc2-waveform   0    ; 0=saw, 1=pulse
+   osc2-pulsewidth 0.50 ;
+   osc2-semi       0    ; semitone offset
+   mix             0.00 ; mix osc1/osc2
+   cutoff          1.0  ; 0.0 - 1.0
+   attack          0.150
+   release         0.015
+   master-volume   0.0  ; volume in dB
+   gate            1    ; note-on/off
+   out-bus         0    ; output bus
+   ]
+  (let [note1-freq  (midicps note)
+        note2-freq  (midicps (+ note osc2-semi))
+        osc1-saw    (lf-saw note1-freq)
+        osc1-pulse  (pulse note1-freq osc1-pulsewidth)
+        osc1        (select osc1-waveform [osc1-saw osc1-pulse])
+        osc2-saw    (lf-saw note2-freq)
+        osc2-pulse  (pulse note2-freq osc2-pulsewidth)
+        osc2        (select osc2-waveform [osc2-saw osc2-pulse])
+        osc         (+ (* mix osc2) (* (- 1 mix) osc1))
+        osc-stereo  [osc osc]
+        filt-stereo (lpf osc-stereo (lin-exp cutoff 0.0 1.0 20.0 20000.0))
+        env1        (env-gen (asr attack 1.0 release) :gate gate :action FREE)
+        master      (pow 10 (/ master-volume 20)) ; dB -> V
         ]
-    (out out-bus (* master env1 [filt filt]))))
+    (out out-bus (* master env1 filt-stereo))))
 
 (comment
 
@@ -47,20 +58,20 @@
   ;; On your own 3
   ;; Bass
   (def mpp (midi-poly-player (partial syntorial-synth
-                                      :osc1-waveform 1
+                                      :osc1-waveform   1
                                       :osc1-pulsewidth 0.4
-                                      :cutoff        0.4
-                                      :master-volume 8.0
-                                      :attack        0.005
-                                      :release       0.25
+                                      :cutoff          0.4
+                                      :master-volume   8.0
+                                      :attack          0.005
+                                      :release         0.25
                                       )))
   ;; Lead
   (def mpp (midi-poly-player (partial syntorial-synth
-                                      :osc1-waveform 0
-                                      :cutoff        0.8
-                                      :master-volume -3.0
-                                      :attack        0.005
-                                      :release       0.5
+                                      :osc1-waveform   0
+                                      :cutoff          0.8
+                                      :master-volume  -3.0
+                                      :attack          0.005
+                                      :release         0.5
                                       )))
   ;; Pad
   (def mpp (midi-poly-player (partial syntorial-synth
@@ -70,6 +81,19 @@
                                       :master-volume   6.0
                                       :attack          2.5
                                       :release         1.5
+                                      )))
+
+
+  ;; Adding 2nd Oscillator
+  (def mpp (midi-poly-player (partial syntorial-synth
+                                      :osc1-waveform   0
+                                      :osc2-waveform   0
+                                      :osc2-semi       19
+                                      :mix             0.5
+                                      :cutoff          0.8
+                                      :master-volume  -3.0
+                                      :attack          0.005
+                                      :release         0.5
                                       )))
 
   (midi-player-stop)
